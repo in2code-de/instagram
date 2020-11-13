33406e8d2765adeac0992e6d87e9825bab54eca3
Alex Kellner <alexander.kellner@in2code.de>
2020-11-13 20:59:17 +0100

    [TASK] Add limit for FE output and store files again

--- Classes/Domain/Service/FetchFeed.php
+++ Classes/Domain/Service/FetchFeed.php
@@ -3,6 +3,7 @@ declare(strict_types=1);
 namespace In2code\Instagram\Domain\Service;
 
 use In2code\Instagram\Exception\FetchCouldNotBeResolvedException;
+use In2code\Instagram\Utility\FileUtility;
 use TYPO3\CMS\Core\Http\RequestFactory;
 use TYPO3\CMS\Core\Utility\GeneralUtility;
 
@@ -22,10 +23,24 @@ class FetchFeed
     protected $profileUri = 'https://www.instagram.com/%s/?__a=1';
 
     /**
+     * @var string
+     */
+    protected $imageFolder = 'typo3temp/assets/tx_instagram/';
+
+    /**
+     * @var bool
+     */
+    protected $storeImages = true;
+
+    /**
      * @var RequestFactory
      */
     protected $requestFactory;
 
+    /**
+     * FetchFeed constructor.
+     * @param RequestFactory|null $requestFactory
+     */
     public function __construct(RequestFactory $requestFactory = null)
     {
         $this->requestFactory = $requestFactory ?: GeneralUtility::makeInstance(RequestFactory::class);
@@ -47,7 +62,9 @@ class FetchFeed
             );
         }
 
-        return $configuration['data']['user']['edge_owner_to_timeline_media']['edges'];
+        $feed = $configuration['data']['user']['edge_owner_to_timeline_media']['edges'];
+        $this->persistImages($feed);
+        return $feed;
     }
 
     /**
@@ -79,5 +96,47 @@ class FetchFeed
         }
 
         return json_decode($request->getBody()->getContents(), true);
+    }
+
+    /**
+     * @param array $configuration
+     * @return array
+     * @throws FetchCouldNotBeResolvedException
+     */
+    protected function persistImages(array $configuration): array
+    {
+        if ($this->storeImages) {
+            $path = GeneralUtility::getFileAbsFileName($this->imageFolder);
+            FileUtility::createFolderIfNotExists($path);
+
+            foreach ($configuration as $item) {
+                $shortcode = $item['node']['shortcode'];
+                $imageContent = $this->getImageContent($item['node']['display_url']);
+                $pathAndName = GeneralUtility::getFileAbsFileName($this->imageFolder) . $shortcode . '.jpg';
+                GeneralUtility::writeFile($pathAndName, $imageContent, true);
+            }
+        }
+        return $configuration;
+    }
+
+    /**
+     * @param string $url
+     * @return string
+     * @throws FetchCouldNotBeResolvedException
+     */
+    protected function getImageContent(string $url): string
+    {
+        $content = '';
+        try {
+            $response = $this->requestFactory->request($url);
+            if ($response->getStatusCode() === 200) {
+                $content = $response->getBody()->getContents();
+            } else {
+                throw new FetchCouldNotBeResolvedException('Image could not be fetched from ' . $url, 1588947571);
+            }
+        } catch (\Exception $exception) {
+            throw new FetchCouldNotBeResolvedException($exception->getMessage(), 1588947539);
+        }
+        return $content;
     }
 }
