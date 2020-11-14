7c695407cb7beea1815b704ed49b0225d98c0327
Alex Kellner <alexander.kellner@in2code.de>
2020-11-14 14:01:12 +0100

    [BUGFIX] Add missing comma in sql structure for table tx_instagram_feed

--- ext_tables.sql
+++ ext_tables.sql
@@ -2,7 +2,7 @@ CREATE TABLE tx_instagram_feed (
     uid int(11) NOT NULL auto_increment,
     import_date int(11) unsigned DEFAULT '0' NOT NULL,
     username varchar(255) DEFAULT '' NOT NULL,
-    data mediumtext NOT NULL
+    data mediumtext NOT NULL,
 
     PRIMARY KEY (uid)
 );
